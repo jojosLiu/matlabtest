21ea722be372971a26779617a887250665195428
jojo <2318410084@qq.com>
2021-07-22 14:02:15 +0800

    解决matlab函数显示未定义的bug！要注意数据类型都是double

--- src/main/java/com/example/matlabtest/MatlabtestApplication.java
+++ src/main/java/com/example/matlabtest/MatlabtestApplication.java
@@ -1,9 +1,11 @@
 package com.example.matlabtest;
 
+
+import ins_fun1.BlockGrover;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.mathworks.toolbox.javabuilder.*;
-import block_grover_matlab.BlockGrover;
+
 
 import java.util.Arrays;
 import java.util.LinkedList;
@@ -18,19 +20,23 @@ public class MatlabtestApplication {
 
 
         MWNumericArray c = null; // 存放x值的数组
+
         try {
             BlockGrover blockGrover = new BlockGrover();
 
             double[] gCovArr = { 20,20,22,25,25};
-
-
-
-
+            double m=5;
+            double n=10;
             c = new MWNumericArray(gCovArr,MWClassID.DOUBLE);
-            Object[] result = blockGrover.ins_fun1(1,c);
+            Object[] result1 = blockGrover.ins_fun1(1,m,n,c);
             double re ;
 
-            System.out.println(Arrays.toString(result));
+//[maxk,psi,C,Emax,x,y,E,opt,k,sumP] = algorithmC(m,n);
+
+//            Object[] result = blockGrover.algorithmC(10,m,n);
+
+
+            System.out.println(Arrays.toString(result1));
 
 
         }catch (MWException e) {
